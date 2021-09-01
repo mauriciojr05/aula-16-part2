8df76cd4f628e945eed7998cc26ef958cfb7499b
MAURICIO LUZ STOFFEL JUNIOR <mauricio.junior1@alunos.sc.senac.br>
2021-09-01 19:57:46 -0300

    coloquei as pastas da parte2 da aula 16

---
+++ IHM/src/ihm/D1.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ihm;
+
+/**
+ *
+ * @author mauricio.junior1
+ */
+public class D1 {
+    //Arraylist<String>da;
+    private String A1;
+    private String A2;
+    private String A3;
+
+
+
+    public String getA1() {
+        return A1;
+    }
+
+    public void setA1(String A1) {
+        this.A1 = A1;
+    }
+
+    public String getA2() {
+        return A2;
+    }
+
+    public void setA2(String A2) {
+        this.A2 = A2;
+    }
+
+    public String getA3() {
+        return A3;
+    }
+
+    public void setA3(String A3) {
+        this.A3 = A3;
+    }
+
+    public D1(String A1, String A2, String A3) {
+        this.A1 = A1;
+        this.A2 = A2;
+        this.A3 = A3;
+    }
+
+    private static class Arraylist<T> {
+
+        public Arraylist() {
+        }
+    }
+}
